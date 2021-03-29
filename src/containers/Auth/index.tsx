f40d2860c2eb5d37bdfeb7bb8c48a29a76c4844e
Roman Fomenko <r.fomenko@artk.ai>
2021-03-29 10:12:50 +0300

    add declare procredit-bank-design-system

--- src/containers/Auth/index.tsx
+++ src/containers/Auth/index.tsx
@@ -1,6 +1,15 @@
 import React, { FC } from 'react'
+import { Row } from 'procredit-bank-design-system'
 import { Wrapper } from './style'
 
-const AuthLayout: FC = ({ children }) => <Wrapper>{children}</Wrapper>
+const AuthLayout: FC = ({ children }) => {
+
+  return (
+    <Wrapper>
+      <Row>test row</Row>
+      {children}
+    </Wrapper>
+  )
+}
 
 export default AuthLayout
---
+++ src/types/procredit-bank-design-system.d.ts
@@ -0,0 +1,1 @@
+declare module 'procredit-bank-design-system';
