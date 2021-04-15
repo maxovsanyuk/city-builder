cdc4950e509a25442509cca340d60647605ebd13
Roman Zhuravlov <r.zhuravlev@artk.ai>
2021-04-15 13:16:09 +0300

    Remove status translations

--- src/settings/languages/locales/en.ts
+++ src/settings/languages/locales/en.ts
@@ -6,10 +6,6 @@ const translations = {
   'welcome-to-my-procredit': 'Welcome to MyProcredit',
   '404-not-found': '404 | Not found',
   'back-to-main-page': 'Back to main page',
-  'authorizations.status.new': 'New',
-  'authorizations.status.eba': 'EBA access',
-  'authorizations.status.no-eba': 'No EBA access',
-  'authorizations.status.dismissed': 'Dismissed',
 }
 
 export default translations
