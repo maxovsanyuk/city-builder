7ac41ec58ef9220ac24131693827836a34eba478
Roman Zhuravlov <r.zhuravlev@artk.ai>
2021-04-15 13:15:55 +0300

    Fix color import

--- src/containers/Platform/Sider.tsx
+++ src/containers/Platform/Sider.tsx
@@ -1,13 +1,15 @@
 import React, { FC, useMemo } from 'react'
 import styled from 'styled-components'
 import { useLocation } from 'react-router-dom'
+import Colors from 'procredit-bank-design-system/modules/colors'
 import Layout from 'procredit-bank-design-system/modules/layout'
 import Menu, { IMenuItem, IItem } from 'components/Common/Menu'
 
+const { gray } = Colors
 const { Sider } = Layout
 
 const StyledSider = styled(Sider).attrs({ theme: 'light' })`
-  border-right: 2px solid var(--color-gray-3);
+  border-right: 2px solid ${gray[3]};
   & > .ant-layout-sider-children {
     padding-top: 20px;
     padding-bottom: 20px;
