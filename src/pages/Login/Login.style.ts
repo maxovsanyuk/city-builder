2d41a6806930bf9d2e4814b1582790670f0ff235
MaksOvs <m.ovsianiuk@artk.ai>
2021-04-16 18:55:28 +0300

    Log-in page 1.3

--- src/pages/Login/Login.style.ts
+++ src/pages/Login/Login.style.ts
@@ -5,10 +5,10 @@ export const Wrapper = styled.div`
   height: 460px;
   background: ${({ theme }) => theme.colors.white};
   border-radius: 16px;
-  padding: 40px;
+  padding: 60px 40px;
 
   .h-4 {
-    margin: 30px 0;
+    margin: 40px 0 30px 0;
   }
 
   form {
