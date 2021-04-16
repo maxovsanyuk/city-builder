1b3c9e0729e6b66003bc311375852d01c3196005
MaksOvs <m.ovsianiuk@artk.ai>
2021-04-16 13:22:47 +0300

    Log-in page 1.2

--- src/components/Common/LangSwitcher/index.tsx
+++ src/components/Common/LangSwitcher/index.tsx
@@ -1,38 +1,54 @@
-// import React, { FC, FormEvent } from 'react'
-// import { Wrapper } from './style'
-//
-// // procredit-bank-design-system
-//
-// import Typography from 'procredit-bank-design-system/modules/typography'
-// import Icons from 'procredit-bank-design-system/modules/icons'
-// import Menu from 'procredit-bank-design-system/modules/menu'
-// const { Item } = Menu
-// const { DownOutlined } = Icons
-// const { Text } = Typography
-// //
-// // const ButtonInner = styled.span`
-// //   text-transform: uppercase;
-// //   & .anticon {
-// //     font-size: 12px;
-// //     width: 24px;
-// //   }
-// // `
-//
-// {
-//   /*<<<<<<< HEAD*/
-// }
-// {
-//   /*export const LangSwitcher: FC = () => {*/
-// }
-// {
-//   /*  const { ref, isComponentVisible, setIsComponentVisible } = useComponentVisible()*/
-// }
-// {
-//   /*  const { lang } = useSelector(({ common }) => common)*/
-// }
-// {
-//   /*=======*/
-// }
+import React, { FC, FormEvent } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import * as CommonActions from 'store/common/actions'
+
+import { Wrapper } from './style'
+
+// procredit-bank-design-system
+
+import Typography from 'procredit-bank-design-system/modules/typography'
+import Icons from 'procredit-bank-design-system/modules/icons'
+const { DownOutlined } = Icons
+const { Text } = Typography
+
+const LANGUAGES = ['en', 'ru']
+
+export const LangSwitcher: FC = () => {
+  const { ref, isComponentVisible, setIsComponentVisible } = useComponentVisible()
+  const { lang } = useSelector(({ common }) => common)
+
+  const dispatch = useDispatch()
+
+  const changeLang = (event: FormEvent, lang: string) => {
+    event.preventDefault()
+    dispatch(CommonActions.changeLangAction({ lang }))
+  }
+
+  return (
+    <Wrapper>
+      <div className="current-lang-box" onClick={() => setIsComponentVisible(pr => !pr)} ref={ref}>
+        <Text className="lang">{lang}</Text>
+        <DownOutlined />
+
+        {isComponentVisible && (
+          <div className="options-cont">
+            {LANGUAGES.map(el => (
+              <button
+                className={`option ${lang === el && 'selected-lang'}`}
+                key={el}
+                disabled={lang === el}
+                onClick={e => changeLang(e, el)}
+              >
+                {el}
+              </button>
+            ))}
+          </div>
+        )}
+      </div>
+    </Wrapper>
+  )
+}
+
 // {
 //   /*const fancyLang = (lang = '') => {*/
 // }
@@ -50,52 +66,4 @@
 // }
 // {
 //   /*}*/
-// }
-// {
-//   /*>>>>>>> 737fdcd82b3c349b4635a1d95770d3413cd503e8*/
-// }
-//
-// // // Render language selector
-// // interface LanguageSwitcherProps {}
-// // const LanguageSwitcher: FC<LanguageSwitcherProps> = () => {
-// //   const dispatch = useDispatch()
-// //
-// // <<<<<<< HEAD
-// //   const changeLang = (event: FormEvent, lang: string) => {
-// //     event.preventDefault()
-// //     dispatch(CommonActions.changeLangAction({ lang }))
-// //   }
-//
-// export const LangSwitcher: FC = () => {
-//   const { ref, isComponentVisible, setIsComponentVisible } = useComponentVisible()
-//   const { lang } = useSelector(({ common }) => common)
-//
-//   const changeLang = (event: FormEvent, lang: string) => {
-//     event.preventDefault()
-//     dispatch(CommonActions.changeLangAction({ lang }))
-//   }
-//
-//   return (
-//     <Wrapper>
-//       <div className="current-lang-box" onClick={() => setIsComponentVisible(pr => !pr)} ref={ref}>
-//         <Text className="lang">{lang}</Text>
-//         <DownOutlined />
-//
-//         {isComponentVisible && (
-//           <div className="options-cont">
-//             {LANGUAGES.map(el => (
-//               <button
-//                 className={`option ${lang === el && 'selected-lang'}`}
-//                 key={el}
-//                 disabled={lang === el}
-//                 onClick={e => changeLang(e, el)}
-//               >
-//                 {el}
-//               </button>
-//             ))}
-//           </div>
-//         )}
-//       </div>
-//     </Wrapper>
-//   )
 // }
