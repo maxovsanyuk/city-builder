bc31055988279dde328667c61f24fead909bbc53
MaksOvs <m.ovsianiuk@artk.ai>
2021-04-28 12:26:14 +0300

    fix pr issue

--- src/pages/Authorizations/Accounts/Accounts.tsx
+++ src/pages/Authorizations/Accounts/Accounts.tsx
@@ -8,7 +8,7 @@ const Accounts: FC<AccountsProps> = () => {
   const { accounts, loading } = useAccounts()
 
   return (
-    <Card title="EditAccountComponent" size="small">
+    <Card title="Index" size="small">
       <AccountsTable data={accounts} loading={loading} />
     </Card>
   )
--- src/pages/Authorizations/EditAccount/index.tsx
+++ src/pages/Authorizations/EditAccount/index.tsx
@@ -1,45 +1,107 @@
-import { FC } from 'react'
+import React, { FC, useState, useEffect } from 'react'
+import { Link, useParams } from 'react-router-dom'
+import { history } from '../../../store/configureStore'
 import { useIntl } from 'react-intl'
-import { Link } from 'react-router-dom'
 
-// Components
+import { Wrapper } from './styles'
 
-import ContentHeader from 'components/Common/ContentHeader'
-import Breadcrumb from 'components/Common/Breadcrumb'
-import EditAccountComponent from './EditAccountComponent'
+import Card from 'procredit-bank-design-system/modules/card'
+import Form from 'procredit-bank-design-system/modules/form'
+import InputAmount from 'procredit-bank-design-system/modules/input-amount'
+import Typography from 'procredit-bank-design-system/modules/typography'
+import Input from 'procredit-bank-design-system/modules/input'
+import Button from 'procredit-bank-design-system/modules/button'
+import Message from 'procredit-bank-design-system/modules/message'
 
-// procredit-bank-design-system
-
-import Icons from 'procredit-bank-design-system/modules/icons'
-
-const { Item } = Breadcrumb
-const { HomeFilled } = Icons
+const { Title } = Typography
 
 interface AccountsProps {}
+interface ParamsProps {
+  accountId: string | undefined
+}
+
 const EditAccount: FC<AccountsProps> = () => {
+  const [form] = Form.useForm()
   const { formatMessage } = useIntl()
+  const { accountId }: ParamsProps = useParams()
+
+  const [, forceUpdate] = useState<any>()
+
+  const onFinish = (values: Record<string, string | number | unknown>) => {
+    console.log(values, 'values')
+
+    Message.success({ content: `${formatMessage({ id: 'processing.complete' })}`, duration: 2 })
+    history.push('/authorizations/accounts')
+  }
+
+  // To disable submit button at the beginning.
+  useEffect(() => {
+    forceUpdate({})
+  }, [])
 
   return (
-    <>
-      <ContentHeader>
-        <Breadcrumb>
-          <Item>
-            <Link to="/">
-              <HomeFilled /> <span className="breadcrumb-item-text">{formatMessage({ id: 'home' })}</span>
-            </Link>
-          </Item>
-          <Item>
-            <Link to="/authorizations">{formatMessage({ id: 'managing.authorizations' })}</Link>
-          </Item>
-          <Item>
-            <Link to="/authorizations/accounts">{formatMessage({ id: 'accounts' })}</Link>
-          </Item>
+    <Wrapper>
+      <Card className="card" title={formatMessage({ id: 'edit' })}>
+        <div className="text-cont">
+          <Title level={5} className="secondary-title">
+            {formatMessage({ id: 'account.number' })} :
+          </Title>
+          <Title level={5} style={{ marginLeft: 10 }}>
+            {accountId}
+          </Title>
+        </div>
 
-          <Item>{formatMessage({ id: 'edit' })}</Item>
-        </Breadcrumb>
-      </ContentHeader>
-      <EditAccountComponent />
-    </>
+        <div className="text-cont">
+          <Title level={5} className="secondary-title">
+            {formatMessage({ id: 'account.type' })} :
+          </Title>
+          <Title level={5} style={{ marginLeft: 10 }}>
+            Current type
+          </Title>
+        </div>
+
+        <Form form={form} name="horizontal_login" layout="vertical" onFinish={onFinish}>
+          <div className="form-items-box">
+            <Form.Item
+              label={formatMessage({ id: 'limit.amount' })}
+              name="limitAmount"
+              rules={[{ required: true, message: formatMessage({ id: 'please.input.limit.amount' }) }]}
+            >
+              <InputAmount />
+            </Form.Item>
+            <Form.Item
+              label={formatMessage({ id: 'required.authorization' })}
+              name="requiredAuthorizationCounter"
+              rules={[{ required: true, message: formatMessage({ id: 'please.input.required.authorization' }) }]}
+            >
+              <Input type="number" />
+            </Form.Item>
+          </div>
+
+          <Form.Item shouldUpdate={true}>
+            {() => (
+              <div style={{ display: 'flex' }}>
+                <Link to="/authorizations/accounts">
+                  <Button type="primary">{formatMessage({ id: 'cancel' })}</Button>
+                </Link>
+
+                <Button
+                  type="primary"
+                  htmlType="submit"
+                  style={{ marginLeft: 15 }}
+                  disabled={
+                    !form.isFieldsTouched(true) ||
+                    form.getFieldsError().filter(({ errors }: any) => errors.length).length
+                  }
+                >
+                  {formatMessage({ id: 'save' })}
+                </Button>
+              </div>
+            )}
+          </Form.Item>
+        </Form>
+      </Card>
+    </Wrapper>
   )
 }
 
