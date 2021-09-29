10d8d6f57bd62f28b8d65e6e94440902ea4cafa1
Maksym Ovsianiuk <maksymovsianiuk@Maksyms-MacBook-Pro.local>
2021-09-29 14:42:21 +0200

    Refactoring

--- src/entities/ui/organisms/pages/advertisement/country-location-content/index.tsx
+++ src/entities/ui/organisms/pages/advertisement/country-location-content/index.tsx
@@ -2,8 +2,8 @@ import { Wrapper } from './styles'
 import { useIntl } from 'react-intl'
 import { useParams } from 'react-router-dom'
 import { Typography } from '@material-ui/core'
-import { GoBackBtn, AdvertisementsBox } from 'shared/ui/molecules'
 import { advertisementsConfig } from 'shared/mock/advertisements'
+import { GoBackBtn, AdvertisementsBox } from 'shared/ui/molecules'
 
 export const CountryLocationAdvertisementContent = () => {
   const { formatMessage } = useIntl()
